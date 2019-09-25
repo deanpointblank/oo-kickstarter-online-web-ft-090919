db74bd48762f42ec7840872329e4c91b7e017583
Dean L Betty <deanlloyd64@gmail.com>
2019-09-25 17:53:49 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
   end
   
   def back_project(project)
-    unless !@backed_projects.include?(self)
+    unless @backed_projects.include?(self)
       @backed_projects << project
       project.add_backer(self)
     end
